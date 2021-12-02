8ab738f3b6aef4839a30e0fb028d290a3d5b02f2
Jade <jade@witchof.space>
2021-12-02 06:37:54 -0500

    no more if-lets, we and-then now

--- rust/src/bin/day02.rs
+++ rust/src/bin/day02.rs
@@ -9,27 +9,17 @@ fn main() -> Result<(), io::Error> {
             (0i64, 0i64, 0i64, 0i64),
             |(horiz, p1_depth, p2_depth, aim), line| {
                 let mut toks = line.split(' ');
-                if let Some(dir) = toks.next() {
-                    if let Some(x) = toks.next() {
-                        if let Ok(x) = x.parse::<i64>() {
-                            Some(match dir {
-                                "forward" => (horiz + x, p1_depth, p2_depth + aim * x, aim),
-                                "down" => (horiz, p1_depth + x, p2_depth, aim + x),
-                                "up" => (horiz, p1_depth - x, p2_depth, aim - x),
-                                _ => unimplemented!(),
-                            })
-                        } else {
-                            None
-                        }
-                    } else {
-                        None
-                    }
-                } else {
-                    None
-                }
+                toks.next().zip(toks.next()).and_then(|(dir, x)| {
+                    x.parse().ok().and_then(|x: i64| match dir {
+                        "forward" => Some((horiz + x, p1_depth, p2_depth + aim * x, aim)),
+                        "down" => Some((horiz, p1_depth + x, p2_depth, aim + x)),
+                        "up" => Some((horiz, p1_depth - x, p2_depth, aim - x)),
+                        _ => None,
+                    })
+                })
             },
         )
-        .expect("Error");
+        .unwrap();
     println!("Part 1 result: {}", horiz * p1_depth);
     println!("Part 2 result: {}", horiz * p2_depth);
     Ok(())
