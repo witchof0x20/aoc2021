1466618d7141f22379d199debf823d4eb847e74b
Jade <jade@witchof.space>
2021-12-02 07:33:15 -0500

    error handling for day 2 in full

--- rust/src/bin/day01.rs
+++ rust/src/bin/day01.rs
@@ -1,4 +1,5 @@
 use std::io::{self, BufRead};
+use std::str::FromStr;
 
 enum WindowState {
     L0,
@@ -7,46 +8,55 @@ enum WindowState {
     L3,
     L4,
 }
+#[derive(Debug)]
+enum Error {
+    IO(io::Error),
+    Parse(usize, <u64 as FromStr>::Err),
+}
 
-fn main() -> Result<(), io::Error> {
+fn main() -> Result<(), Error> {
     use WindowState::*;
     let (_, _, part1_result, part2_result) = io::stdin()
         .lock()
         .lines()
-        .flatten()
-        .map(|line| line.parse())
-        .flatten()
-        .fold(
+        .enumerate()
+        .map(|(line_num, line)| {
+            line.map_err(Error::IO)
+                .and_then(|line| line.parse().map_err(|err| Error::Parse(line_num, err)))
+        })
+        .try_fold(
             ([0, 0, 0, 0], L0, 0, 0),
-            |([a, b, c, d], length, count1, count2), x: u64| match length {
-                // Window is empty, append the first element
-                L0 => ([x, 0, 0, 0], L1, 0, 0),
-                // Window has 1 element, append the second and compare
-                L1 => ([a, x, 0, 0], L2, (x > a).into(), 0),
-                // Window has 2 elements, append the third and compare
-                L2 => ([a, b, x, 0], L3, count1 + usize::from(x > b), 0),
-                // Window has 3 elements, append the fourth and compare for
-                // both problems
-                // We compare each element with the element 3 backwards because
-                // b + c + d > a + b + c is equivalent to
-                // d > a
-                L3 => (
-                    [a, b, c, x],
-                    L4,
-                    count1 + usize::from(x > c),
-                    (x > a).into(),
-                ),
-                // Window has 4 elements. Shift left, compare for both problems
-                // This is the terminal state, and will continue for all future
-                // elements
-                L4 => (
-                    [b, c, d, x],
-                    L4,
-                    count1 + usize::from(x > d),
-                    count2 + usize::from(x > b),
-                ),
+            |([a, b, c, d], length, count1, count2), x: Result<u64, Error>| {
+                x.map(|x| match length {
+                    // Window is empty, append the first element
+                    L0 => ([x, 0, 0, 0], L1, 0, 0),
+                    // Window has 1 element, append the second and compare
+                    L1 => ([a, x, 0, 0], L2, (x > a).into(), 0),
+                    // Window has 2 elements, append the third and compare
+                    L2 => ([a, b, x, 0], L3, count1 + usize::from(x > b), 0),
+                    // Window has 3 elements, append the fourth and compare for
+                    // both problems
+                    // We compare each element with the element 3 backwards because
+                    // b + c + d > a + b + c is equivalent to
+                    // d > a
+                    L3 => (
+                        [a, b, c, x],
+                        L4,
+                        count1 + usize::from(x > c),
+                        (x > a).into(),
+                    ),
+                    // Window has 4 elements. Shift left, compare for both problems
+                    // This is the terminal state, and will continue for all future
+                    // elements
+                    L4 => (
+                        [b, c, d, x],
+                        L4,
+                        count1 + usize::from(x > d),
+                        count2 + usize::from(x > b),
+                    ),
+                })
             },
-        );
+        )?;
 
     println!("Part 1: {}", part1_result);
     println!("Part 2: {}", part2_result);
--- rust/src/bin/day02.rs
+++ rust/src/bin/day02.rs
@@ -1,21 +1,42 @@
 use std::io::{self, BufRead};
+use std::str::FromStr;
 
-fn main() -> Result<(), io::Error> {
+#[derive(Debug)]
+enum Error {
+    IO(io::Error),
+    MissingFirstToken,
+    InvalidDirection(String),
+    MissingSecondToken,
+    Parse(usize, <u64 as FromStr>::Err),
+}
+fn main() -> Result<(), Error> {
     let (horiz, p1_depth, p2_depth, _) = io::stdin()
         .lock()
         .lines()
-        .flatten()
+        .map(|line| line.map_err(Error::IO))
+        .enumerate()
         .try_fold(
             (0i64, 0i64, 0i64, 0i64),
-            |(horiz, p1_depth, p2_depth, aim), line| {
-                let mut toks = line.split(' ');
-                toks.next().zip(toks.next()).and_then(|(dir, x)| {
-                    x.parse().ok().and_then(|x: i64| match dir {
-                        "forward" => Some((horiz + x, p1_depth, p2_depth + aim * x, aim)),
-                        "down" => Some((horiz, p1_depth + x, p2_depth, aim + x)),
-                        "up" => Some((horiz, p1_depth - x, p2_depth, aim - x)),
-                        _ => None,
-                    })
+            |(horiz, p1_depth, p2_depth, aim), (line_num, line)| {
+                line.and_then(|line| {
+                    let mut toks = line.split(' ');
+                    toks.next()
+                        .ok_or_else(|| Error::MissingFirstToken)
+                        .and_then(|dir| {
+                            toks.next()
+                                .map(|x| (dir, x))
+                                .ok_or_else(|| Error::MissingSecondToken)
+                        })
+                        .and_then(|(dir, x)| {
+                            x.parse()
+                                .map_err(|err| Error::Parse(line_num, err))
+                                .and_then(|x: i64| match dir {
+                                    "forward" => Ok((horiz + x, p1_depth, p2_depth + aim * x, aim)),
+                                    "down" => Ok((horiz, p1_depth + x, p2_depth, aim + x)),
+                                    "up" => Ok((horiz, p1_depth - x, p2_depth, aim - x)),
+                                    other => Err(Error::InvalidDirection(other.to_owned())),
+                                })
+                        })
                 })
             },
         )
