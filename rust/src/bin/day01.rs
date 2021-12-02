494e3a31cfe73ca04de74531da9edd5bdfe729ca
Jade <jade@witchof.space>
2021-12-02 06:58:16 -0500

    single iterator day1

--- rust/src/bin/day01.rs
+++ rust/src/bin/day01.rs
@@ -1,21 +1,40 @@
 use std::io::{self, BufRead};
 
+enum WindowState {
+    E0,
+    E1,
+    E2,
+    E3,
+    E4,
+}
 fn main() -> Result<(), io::Error> {
-    let input: Vec<u64> = io::stdin()
+    use WindowState::*;
+    let (_, _, part1_result, part2_result) = io::stdin()
         .lock()
         .lines()
         .flatten()
         .map(|line| line.parse())
         .flatten()
-        .collect();
-
-    let (part1_result, part2_result) = input
-        .iter()
-        .zip(input.iter().skip(1))
-        .zip(input.iter().skip(3).chain(&[0, 0]))
-        .fold((0, 0), |(count1, count2), ((a, b), c)| {
-            (count1 + usize::from(b > a), count2 + usize::from(c > a))
-        });
+        .fold(
+            ([0, 0, 0, 0], E0, 0usize, 0usize),
+            |([a, b, c, d], win_state, count1, count2), x: u64| match win_state {
+                E0 => ([x, 0, 0, 0], E1, 0, 0),
+                E1 => ([a, x, 0, 0], E2, (x > a).into(), 0),
+                E2 => ([a, b, x, 0], E3, count1 + usize::from(x > b), 0),
+                E3 => (
+                    [a, b, c, x],
+                    E4,
+                    count1 + usize::from(x > c),
+                    (x > a).into(),
+                ),
+                E4 => (
+                    [b, c, d, x],
+                    E4,
+                    count1 + usize::from(x > d),
+                    count2 + usize::from(x > b),
+                ),
+            },
+        );
 
     println!("Part 1: {}", part1_result);
     println!("Part 2: {}", part2_result);
