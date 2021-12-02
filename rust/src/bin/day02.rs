b2b6ad4a624c64945c54e4c06c7f5809bba5bf0f
Jade <jade@witchof.space>
2021-12-02 05:32:31 -0500

    Day2 fix

--- rust/src/bin/day02.rs
+++ rust/src/bin/day02.rs
@@ -1,39 +1,25 @@
 use std::io::{self, BufRead};
 
-enum Direction {
-    Forward,
-    Down,
-    Up,
-}
 fn main() -> Result<(), io::Error> {
-    let input: Vec<_> = io::stdin()
-        .lock()
-        .lines()
-        .map(|line| {
+    let (p1_horiz, p1_depth, p2_horiz, p2_depth, _) = io::stdin().lock().lines().fold(
+        (0i64, 0i64, 0i64, 0i64, 0i64),
+        |(p1_horiz, p1_depth, p2_horiz, p2_depth, aim), line| {
             let line = line.unwrap();
             let mut toks = line.split(' ');
-            let dir = match toks.next().unwrap() {
-                "forward" => Direction::Forward,
-                "down" => Direction::Down,
-                "up" => Direction::Up,
+            let dir = toks.next().unwrap();
+            let x: i64 = toks.next().unwrap().parse().unwrap();
+            match dir {
+                "forward" => (
+                    p1_horiz + x,
+                    p1_depth,
+                    p2_horiz + x,
+                    p2_depth + aim * x,
+                    aim,
+                ),
+                "down" => (p1_horiz, p1_depth + x, p2_horiz, p2_depth, aim + x),
+                "up" => (p1_horiz, p1_depth - x, p2_horiz, p2_depth, aim - x),
                 _ => unimplemented!(),
-            };
-            let num: i64 = toks.next().unwrap().parse().unwrap();
-            (dir, num)
-        })
-        .collect();
-    let (p1_horiz, p1_depth, p2_horiz, p2_depth, _) = input.iter().fold(
-        (0i64, 0i64, 0i64, 0i64, 0i64),
-        |(p1_horiz, p1_depth, p2_horiz, p2_depth, aim), (dir, x)| match dir {
-            Direction::Forward => (
-                p1_horiz + x,
-                p1_depth,
-                p2_horiz + x,
-                p2_depth + aim * x,
-                aim,
-            ),
-            Direction::Down => (p1_horiz, p1_depth + x, p2_horiz, p2_depth, aim + x),
-            Direction::Up => (p1_horiz, p1_depth - x, p2_horiz, p2_depth, aim - x),
+            }
         },
     );
     println!("Part 1 result: {}", p1_horiz * p1_depth);
