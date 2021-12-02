632d94e287c7670aee22bc31afb236238ea52546
Jade <jade@witchof.space>
2021-12-02 06:24:47 -0500

    Error handling i guess

--- rust/src/bin/day02.rs
+++ rust/src/bin/day02.rs
@@ -1,20 +1,35 @@
 use std::io::{self, BufRead};
 
 fn main() -> Result<(), io::Error> {
-    let (horiz, p1_depth, p2_depth, _) = io::stdin().lock().lines().flatten().fold(
-        (0i64, 0i64, 0i64, 0i64),
-        |(horiz, p1_depth, p2_depth, aim), line| {
-            let mut toks = line.split(' ');
-            let dir = toks.next().unwrap();
-            let x: i64 = toks.next().unwrap().parse().unwrap();
-            match dir {
-                "forward" => (horiz + x, p1_depth, p2_depth + aim * x, aim),
-                "down" => (horiz, p1_depth + x, p2_depth, aim + x),
-                "up" => (horiz, p1_depth - x, p2_depth, aim - x),
-                _ => unimplemented!(),
-            }
-        },
-    );
+    let (horiz, p1_depth, p2_depth, _) = io::stdin()
+        .lock()
+        .lines()
+        .flatten()
+        .try_fold(
+            (0i64, 0i64, 0i64, 0i64),
+            |(horiz, p1_depth, p2_depth, aim), line| {
+                let mut toks = line.split(' ');
+                if let Some(dir) = toks.next() {
+                    if let Some(x) = toks.next() {
+                        if let Ok(x) = x.parse::<i64>() {
+                            Some(match dir {
+                                "forward" => (horiz + x, p1_depth, p2_depth + aim * x, aim),
+                                "down" => (horiz, p1_depth + x, p2_depth, aim + x),
+                                "up" => (horiz, p1_depth - x, p2_depth, aim - x),
+                                _ => unimplemented!(),
+                            })
+                        } else {
+                            None
+                        }
+                    } else {
+                        None
+                    }
+                } else {
+                    None
+                }
+            },
+        )
+        .expect("Error");
     println!("Part 1 result: {}", horiz * p1_depth);
     println!("Part 2 result: {}", horiz * p2_depth);
     Ok(())
