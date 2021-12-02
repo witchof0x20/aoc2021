a997dcf7ccd8f1c85336d56b1d1d0ee0db650ae9
Jade <jade@witchof.space>
2021-12-02 05:38:06 -0500

    simplified day2 again

--- rust/src/bin/day02.rs
+++ rust/src/bin/day02.rs
@@ -1,28 +1,22 @@
 use std::io::{self, BufRead};
 
 fn main() -> Result<(), io::Error> {
-    let (p1_horiz, p1_depth, p2_horiz, p2_depth, _) = io::stdin().lock().lines().fold(
-        (0i64, 0i64, 0i64, 0i64, 0i64),
-        |(p1_horiz, p1_depth, p2_horiz, p2_depth, aim), line| {
+    let (horiz, p1_depth, p2_depth, _) = io::stdin().lock().lines().fold(
+        (0i64, 0i64, 0i64, 0i64),
+        |(horiz, p1_depth, p2_depth, aim), line| {
             let line = line.unwrap();
             let mut toks = line.split(' ');
             let dir = toks.next().unwrap();
             let x: i64 = toks.next().unwrap().parse().unwrap();
             match dir {
-                "forward" => (
-                    p1_horiz + x,
-                    p1_depth,
-                    p2_horiz + x,
-                    p2_depth + aim * x,
-                    aim,
-                ),
-                "down" => (p1_horiz, p1_depth + x, p2_horiz, p2_depth, aim + x),
-                "up" => (p1_horiz, p1_depth - x, p2_horiz, p2_depth, aim - x),
+                "forward" => (horiz + x, p1_depth, p2_depth + aim * x, aim),
+                "down" => (horiz, p1_depth + x, p2_depth, aim + x),
+                "up" => (horiz, p1_depth - x, p2_depth, aim - x),
                 _ => unimplemented!(),
             }
         },
     );
-    println!("Part 1 result: {}", p1_horiz * p1_depth);
-    println!("Part 2 result: {}", p2_horiz * p2_depth);
+    println!("Part 1 result: {}", horiz * p1_depth);
+    println!("Part 2 result: {}", horiz * p2_depth);
     Ok(())
 }
